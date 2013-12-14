d8acec8229e5945707f913faca99036ee6c7abc9
SketchTurner <mih.dmitry@gmail.com>
2013-12-14 19:27:06 +0400

    configuration file

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# configuration file
+
+import os
+
+base_dir = os.path.normpath(os.path.dirname(__file__))
+data_dir = os.path.join(base_dir, "data")
+img_dir = os.path.join(data_dir, "img")
+pacman_dir = os.path.join(img_dir,"pacman")
