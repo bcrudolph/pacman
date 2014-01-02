5fdbe529e1e589c9e57b3c4aae32b581691c1354
SketchTurner <mih.dmitry@gmail.com>
2014-01-03 01:26:49 +0400

    modifications

---
+++ level001.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from utils import load_image
+import os
+import config
+
+class Level:
+    """The Base Class for Levels"""
+    def getLayout(self):
+        """Get the Layout of the level"""
+        """Returns a [][] list"""
+        pass
+    def getImages(self):
+        """Get a list of all the images used by the level"""
+        """Returns a list of all the images used.  The indices
+        in the layout refer to sprites in the list returned by
+        this function"""
+        pass
+
+class level(Level):
+    """Level 1 of the Pacman Game"""
+
+    def __init__(self):
+        self.PELLET = 0
+        self.BLOCK = 1
+        self.PACMAN = 2
+        self.GWALL = 3
+        self.BLINKY = 4
+        self.INKY = 5
+        self.PINKY = 6
+        self.CLYDE = 7
+	
+    def get_layout(self):
+        return [[9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],\
+                [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],\
+                [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 0, 0, 0, 7, 0, 0, 0, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 0, 1, 1, 3, 1, 1, 0, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 0, 0, 1, 4, 5, 6, 1, 0, 0, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 9],\
+                [9, 1, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1, 0, 0, 1, 9],\
+                [9, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 9],\
+                [9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9],\
+                [9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9],\
+                [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]]
+
+    def get_image_paths(self):
+        block = os.path.join(config.img_dir, "block.png")
+        pellet = os.path.join(config.img_dir, "pellet.png")
+        pacman = os.path.join(config.pacman_dir, "*.png")
+        gwall = os.path.join(config.img_dir, "gwall.png")
+        blinky = os.path.join(config.enemy_dir, "blinky.png")
+        inky = os.path.join(config.enemy_dir, "inky.png")
+        pinky = os.path.join(config.enemy_dir, "pinky.png")
+        clyde = os.path.join(config.enemy_dir, "clyde.png")
+
+        return [pellet, block, pacman, gwall, blinky, pinky, inky, clyde]
+        
